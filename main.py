13d9c4a78cfd76c9e3b56abc67970dff348376a5
Jamison Johnson <jamisonbjohnson1278@gmail.com>
2018-07-23 16:17:29 -0400

    named the selections

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import webapp2
 import jinja2
 import os
+from models import Recommendation
+
+mood = ""
+occasion = ""
+
+inspirational_movies =
 
 the_jinja_environment = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -15,16 +21,24 @@ class WelcomePage(webapp2.RequestHandler):
     def post(self):
         result_template = the_jinja_environment.get_template('templates/result.html')
         self.response.write(welcome_template.render())
+        global mood = self.request.get('mood')
+        global occasion = self.request.get('occasion')
+
 
 class ResultPage(webapp2.RequestHandler):
     def get():
         welcome_template = the_jinja_environment.get_template('templates/result.html')
         self.response.write(welcome_template.render())
 
+        Recommendation.mood = mood
+        Recommendation.occasion = occasion
+
+
+
     def post():
         result_template = the_jinja_environment.get_template('templates/result.html')
         self.response.write(result_template.render())
-            
+
 
 
 
---
+++ models.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import ndb
+
+class Recommendation(ndb.model):
+    mood = ndb.StringProperty()
+    occasion = ndb.StringProperty()
+
+    
