e189d34a23e291200ca0b7d17cf3a2ba4ed0e9b7
Darius Daniel <Darius.Daniel35@gmail.com>
2018-07-24 09:47:22 -0400

    mood

--- models.py
+++ models.py
@@ -5,8 +5,9 @@ class Movie(ndb.Model):
     duration = ndb.IntegerProperty()
     rating = ndb.IntegerProperty()
     description = ndb.StringProperty()
+    mood = ndb.StringProperty()
 
 shrek = Movie(
-    title='Shrek', duration=123, rating=10, description='About an ogre who lives in a swamp.')
+    title='Shrek', duration=123, rating=10, description='About an ogre who lives in a swamp.', mood='casual, silly')
 
 shrek_key = shrek.put()
