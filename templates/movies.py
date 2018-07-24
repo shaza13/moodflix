cbad1e082850dfcdb03e5c2b80f93fb67eaff7e2
Shaza Mehdi <shaza0313@gmail.com>
2018-07-24 09:32:10 -0400

    new db

---
+++ templates/movies.py
@@ -0,0 +1,12 @@
+from google.appengine.ext import ndb
+
+class Movie(ndb.Model):
+    title = ndb.StringProperty()
+    duration = ndb.IntegerProperty()
+    rating = ndb.IntegerProperty()
+    description = ndb.StringProperty()
+
+shrek = Movie(
+    title='Shrek', duration=123, rating=10, description='About an ogre who lives in a swamp.')
+
+shrek_key = shrek.put()
