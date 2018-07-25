0f2d654df68dabe537f26bce22f28afe5da28cfa
Jamison Johnson <jamisonbjohnson1278@gmail.com>
2018-07-25 11:23:44 -0400

    changes

--- main.py
+++ main.py
@@ -43,14 +43,15 @@ class Upload(webapp2.RequestHandler):  # todo: adding to the database each run a
         thor_key = thor.put()
         wake = Movie(title='Before I wake', duration=97, rating=6, description='A couple adopt an orphaned child whose dreams - and nightmares - manifest physically as he sleeps.', mood='gloomy', occasion='halloween based')
         wake_key = wake.put()
-        buddies = Movie(title='Santa Buddies', duration=88, rating=5, description='At the North Pole, Santa Claus (Father Christmas) and his chief dog Santa Paws worry as the whole toy processing system is threatened by the weakening of its magical power source, the icicle drawing on Christmas spirit.', mood='cheerful', occasion='Christmas')
+        santa = Movie(title='Santa Buddies', duration=88, rating=5, description='At the North Pole, Santa Claus (Father Christmas) and his chief dog Santa Paws worry as the whole toy processing system is threatened by the weakening of its magical power source, the icicle drawing on Christmas spirit.', mood='cheerful', occasion='Christmas')
         pounds = Movie(title='Seven Pounds', duration=123, rating=8, description='A man with a fateful secret embarks on an extraordinary journey of redemption by forever changing the lives of seven strangers.', mood='inspirational', occasion='Casual Watching')
+        self.response.write(welcome_template.render())
 
     def post(self):
         mood = self.request.get('mood')
         occasion = self.request.get('occasion')
         result_template = the_jinja_environment.get_template('templates/result.html')
-        self.response.write(welcome_template.render())
+        self.response.write(result_template.render())
 
 
 class ResultPage(webapp2.RequestHandler):
