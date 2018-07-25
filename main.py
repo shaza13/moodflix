a7def60dfe00dbcf79a4bbad2205b70f773db4e7
Jamison Johnson <jamisonbjohnson1278@gmail.com>
2018-07-25 10:13:32 -0400

    ks

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Upload(webapp2.RequestHandler):  # todo: adding to the database each run a
         santa = Movie(title='Santa Buddies', duration=88, rating=5, description='At the North Pole, Santa Claus (Father Christmas) and his chief dog Santa Paws worry as the whole toy processing system is threatened by the weakening of its magical power source, the icicle drawing on Christmas spirit.', mood='cheerful', occasion='Christmas')
         pounds = Movie(title='Seven Pounds', duration=123, rating=8, description='A man with a fateful secret embarks on an extraordinary journey of redemption by forever changing the lives of seven strangers.', mood='inspirational', occasion='Casual Watching')
 
-        # self.response.write(welcome_template.render())
+    def post(self):
         self.redirect("/result")
 
 
